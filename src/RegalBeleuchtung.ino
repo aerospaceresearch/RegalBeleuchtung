64b0af36252da795eac38a6faa52e39ada723bd0
saeugetier <saeugetier@googlemail.com>
2016-12-09 21:32:52 +0100

    added skeleton for main program

---
+++ src/RegalBeleuchtung.ino
@@ -0,0 +1,17 @@
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include <ESP8266mDNS.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
